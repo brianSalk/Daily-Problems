f47327886f246a05fd47ec6149ff1f431753d900
BrianSalk <brian.salkas@gmail.com>
2023-12-18 10:30:02 -0600

    Create how_biased.py

---
+++ how_biased.py
@@ -0,0 +1,45 @@
+# given three coins, how biased mush one of those coins be in order for you to have a 60% chance of getting heads when flipping a random coin?
+
+import random
+
+
+class Coin:
+    def __init__(self, p=0.5):
+        self.probability = p
+
+    def flip(self):
+        return random.random() < self.probability
+
+
+if __name__ == "__main__":
+    goal = .6
+    # create three fair coins
+    coins = [Coin(), Coin(), Coin()]
+    num_trials = 1_000_000
+    num_heads = 0
+    upper = 1
+    lower = 0
+    # do a biary search to find the bias
+    while True:
+        coin_bias = (upper + lower) / 2
+        coins[0].probability = coin_bias
+        num_heads = 0
+        for _ in range(num_trials):
+            coin = random.choice(coins)
+            if coin.flip():
+                num_heads += 1
+        p = num_heads / num_trials
+        # if the bias is really close to 1, then assume no solution
+        if abs(coin_bias - 1) < .0001:
+            print(f"cannot find bias for {goal}")
+            break
+        # allow 10th of a percent error
+        if abs(p - goal) < .001:
+            print(f"probability= {p}, bias= {coin_bias}")
+            break
+        elif p > goal:
+            #print(f'{p} is too high', lower, upper)
+            upper = coin_bias
+        else:
+            #print(f'{p} is too low', lower, upper)
+            lower = coin_bias
---
+++ numbers_with_4_but_not_5.py
@@ -0,0 +1,16 @@
+# how many numbers less than 10000 have 4 but not 5 in them?
+
+
+# to make this easier we can left pad the numbers with 0s
+# so 10 is 0010 and 123 is 0123 etc.
+# 1000 numbers have 4 in each of the 4 digits
+# that means 1000 numbers begin with 4
+# 900 additional numbers have 4 as the second digit
+# 810 additional numbers have 4 as the third digit
+# 729 additional numbers have 4 as the fourth digit
+if __name__ == "__main__":
+    count = 0
+    for i in range(10000):
+        if '4' in str(i) and '5' not in str(i):
+            count += 1
+    print(count)
