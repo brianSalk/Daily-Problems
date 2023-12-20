27395193bf6a18697600856b064f863c15a6dd71
BrianSalk <brian.salkas@gmail.com>
2023-12-20 14:16:19 -0600

    Fix and simplify dna_sequences_with_restrictions.py

--- dna_sequences_with_restrictions.py
+++ dna_sequences_with_restrictions.py
@@ -1,29 +1,20 @@
-# A DNA sequence consists of some combination of zero or more of each A,T,C, and G.
+# A DNA sequence consists of a combination of 0 or more of each A,T,C, and G.
 # each A,T,C, or G represents a nucleotide
 # Given that we know the following about a newly discovered species:
-# -- there may no be five or more adjacent A's in a single gene
+# -- there cannot be more than three adjacent A's in a single gene
 # -- there must be at least one G
 # -- there must be at least two T's
 # how many genes of length 7 nucleotides can there be in this species?
-import random
-from statistics import mean
 from itertools import combinations_with_replacement, permutations
-
-
-def has_sublist(sublist, ls):
-    """starting at each index in ls, check if sublist exists"""
-    for i in range(len(ls)):
-        if ls[i:-1] == sublist:
-            return True
-    return False
-
 
 if __name__ == "__main__":
     ans = set()
+    every = set()
     for comb in combinations_with_replacement(("A", "T", "C", "G"), 7):
         for perm in permutations(comb):
-            if ("G" in perm and perm.count("T") > 1) and not has_sublist(
-                ("A", "A", "A", "A", "A"), perm
-            ):
+            perm = "".join(perm)
+            if ("G" in perm and perm.count("T") > 1) and "AAA" not in perm:
                 ans.add(perm)
+            every.add(perm)
+    print(ans)
     print(len(ans))
