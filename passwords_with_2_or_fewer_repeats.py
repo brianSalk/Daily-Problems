7deb0c1065693190841b9df88ff11c2fc25facc6
BrianSalk <brian.salkas@gmail.com>
2023-12-20 10:45:57 -0600

    Simplify passwords_with_2_or_fewer_repeats.py, add comments

--- passwords_with_2_or_fewer_repeats.py
+++ passwords_with_2_or_fewer_repeats.py
@@ -1,3 +1,6 @@
+# count all 5 digit passwords with 1 or fewer repeated digits
+# this enumeration is supposed to be easy to understand,
+# not efficient or maintainable
 from itertools import permutations
 
 if __name__ == "__main__":
@@ -6,7 +9,4 @@ if __name__ == "__main__":
     for perm in permutations(chars, 5):
         if len(set(perm)) > 3:
             passwords.add("".join(perm))
-        else:
-            #print("".join(perm))
-            ...
     print("num perms:", len(passwords))
