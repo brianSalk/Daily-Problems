9ef99881cb870e79d4b3e180e63191118a7d0c83
BrianSalk <brian.salkas@gmail.com>
2023-12-20 11:04:04 -0600

    Fix bug in permutations_without_4_and_5_adjacent.py

--- perutations_without_4_and_5_adjacent.py
+++ perutations_without_4_and_5_adjacent.py
@@ -1,16 +1,12 @@
+# count the number of permutations of 1-10 where 4 and 5 are not adjacent
 from itertools import permutations
 
 if __name__ == "__main__":
-    numbers = list(range(1, 11))
+    numbers = [str(i) for i in range(1, 11)]
     count = 0
     for perm in permutations(numbers):
-        index_of_4 = perm.index(4)
-        if index_of_4 == 0 and perm[1] == 5:
-            continue
-        elif index_of_4 == 9 and perm[8] == 5:
-            continue
-        if 0 > index_of_4 < 9 and (perm[index_of_4 - 1] == 5 or perm[index_of_4 + 1] == 5):
-            continue
-        count += 1
+        perm = "".join(perm)
+        if "45" not in perm and "54" not in perm:
+            count += 1
 
     print(f'there are {count:,} permutations where 4 and 5 are not adjacent')
