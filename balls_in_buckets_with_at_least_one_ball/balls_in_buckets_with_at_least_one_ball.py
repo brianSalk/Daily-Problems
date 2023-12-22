4f01700979638796db1966d6f98f9e59361ac461
BrianSalk <brian.salkas@gmail.com>
2023-12-22 13:42:38 -0600

    Create balls_in_buckets_with_at_least_one_ball

---
+++ balls_in_buckets_with_at_least_one_ball/balls_in_buckets_with_at_least_one_ball.py
@@ -0,0 +1,18 @@
+from itertools import combinations_with_replacement
+
+if __name__ == '__main__':
+    arrangements = set()
+    balls = 7
+    # fill the rest of the balls
+    for selections in combinations_with_replacement([0,1,2,3], balls):
+        num_buckets = 3
+        buckets = [0] * num_buckets
+        for i in selections:
+            if i < num_buckets:
+                buckets[i] += 1
+        if buckets[0:3].count(0) == 0:
+            arrangements.add(tuple(buckets))
+    for each in arrangements:
+        print(each[0:3]) 
+    print(f'there are {len(arrangements)} ways to put {balls} balls in 4 buckets where each bucket has at least one ball')
+
