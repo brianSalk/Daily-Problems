82214d58e2564ae8ac597cf9800bf33fbdfbb2c4
BrianSalk <brian.salkas@gmail.com>
2023-12-21 19:51:02 -0600

    Remove superfluous comments

--- numbers_with_4_but_not_5/numbers_with_4_but_not_5.py
+++ numbers_with_4_but_not_5/numbers_with_4_but_not_5.py
@@ -1,22 +1,5 @@
-# how many numbers between 0 and 10000 have 4 but not 5 in them?
-
-
-# to make this easier we can left pad the numbers with 0s
-# so 10 is 0010 and 123 is 0123 etc.
-# 1000 numbers have 4 in each of the 4 digits
-# that means 1000 numbers begin with 4
-# 900 additional numbers have 4 as the second digit
-# 810 additional numbers have 4 as the third digit
-# 729 additional numbers have 4 as the fourth digit
-# add those up and you get 3439
-# but we need to subtract the numbers that have 5 and 4 in them
-
-# easy way:
-# 9^4 numbers do not have 4
-# 10,000 - 9^4 = 3439
-
-
-
+# how many numbers between 0 and 10000 have 4 but not 5
+# as at least one of their digits?
 if __name__ == "__main__":
     count = 0
     for i in range(10_000):
