dad8e387069f0946c63ef011a4ab2b6bff6e7a2e
BrianSalk <brian.salkas@gmail.com>
2023-12-29 14:01:36 -0600

    Create breakfast_options

---
+++ breakfast_options/simulate.py
@@ -0,0 +1,29 @@
+# how many breakfasts can i create if i must choose:
+# one or fewer entree:
+#    pancakes
+#    waffles
+#    french toast
+
+# one or fewer drinks:
+#    coffee
+#    tea
+#    orange juice
+#    milk
+
+# one or fewer sides:
+#    breakfast sausage
+#    fruit
+
+if __name__ == '__main__':
+    entrees = ['pancakes', 'waffles', 'french toast']
+    drinks = ['coffee', 'tea', 'orange juice', 'milk']
+    sides = ['breakfast sausage', 'fruit']
+
+    breakfasts = 0
+    for entree in entrees:
+        for drink in drinks:
+            for side in sides:
+                print(entree, drink, side)
+                breakfasts += 1
+
+    print(f'there are {breakfasts} breakfasts')
