ee833f43dfaa6bc81e58f28141a78aa36dd7e519
BrianSalk <brian.salkas@gmail.com>
2023-12-28 22:43:54 -0600

    Change variable names

--- rats_in_cages/simulate.py
+++ rats_in_cages/simulate.py
@@ -1,34 +1,34 @@
 from itertools import combinations_with_replacement, permutations
 
-def q1(num_people, num_rooms):
+def q1(num_people, num_cages):
     ans = set()
     for com in combinations_with_replacement(range(4), 6):
         for perm in permutations(com):
-            rooms = [0] * num_rooms
+            cages = [0] * num_cages
             for i in perm:
-                rooms[i] += 1
-            rooms.sort()
-            ans.add(tuple(rooms))
+                cages[i] += 1
+            cages.sort()
+            ans.add(tuple(cages))
     print(ans)
     print(len(ans))
 
 
-def q2(num_people, num_rooms):
+def q2(num_people, num_cages):
     ans = set()
     for com in combinations_with_replacement(range(4), 6):
         for perm in permutations(com):
-            rooms = [0] * num_rooms
+            cages = [0] * num_cages
             for i in perm:
-                rooms[i] += 1
-            ans.add(tuple(rooms))
+                cages[i] += 1
+            ans.add(tuple(cages))
     print(ans)
     print(len(ans))
 
 if __name__ == "__main__":
-    num_people = 6
-    num_rooms = 4
-    q1(num_people, num_rooms)
-    q2(num_people, num_rooms)
+    num_rats = 6
+    num_cages = 4
+    q1(num_rats, num_cages)
+    q2(num_rats, num_cages)
 
         
 
