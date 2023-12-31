dba595d4d353c35cc69e159baea271a8b7aa1b47
BrianSalk <brian.salkas@gmail.com>
2023-12-31 01:49:00 -0600

    Make simulate.py actually work

--- fish_in_aquariums/simulate.py
+++ fish_in_aquariums/simulate.py
@@ -3,19 +3,30 @@
 
 # The fish are randomly assigned to the aquariums
 # how many ways can all the fish be assigned to the aquariums?
+
+
+# we iterate over all possible combinations of fish
 import itertools
 from collections import Counter, defaultdict
-if __name__ == '__main__':
-    import itertools
-    fish = ['bass'] * 5 + ['bluegill'] * 5
-    aquariums = ['round', 'cubical']
+
+if __name__ == "__main__":
+    fish = ["bass"] * 4 + ["bluegill"] * 4
     ans = set()
-    for fish_perm in itertools.permutations(fish):
-        arrangement = {'round': defaultdict(int),'cube': defaultdict(int)}
-        for aquarium_index in itertools.combinations_with_replacement(['round', 'cube'], 10):
-            for i in range(10):
-                arrangement[aquarium_index[i]][fish_perm[i]] += 1
-            round_dict = frozendict(arrangement['round'])
-    print('There are {} unique combinations'.format(len(unique_combinations)))
-    for c in unique_combinations:
-        print(c)
+    for fish_perms in itertools.permutations(fish):
+        for tanks in itertools.combinations_with_replacement(['round', 'cube'], len(fish)):
+            fish_tank_counts = [0, 0, 0, 0] #round bass, round bluegill, cube bass, cube bluegill
+            for i in range(len(fish)):
+                if tanks[i] == 'round' and fish_perms[i] == 'bass':
+                    fish_tank_counts[0] += 1
+                elif tanks[i] == 'round' and fish_perms[i] == 'bluegill':
+                    fish_tank_counts[1] += 1
+                elif tanks[i] == 'cube' and fish_perms[i] == 'bass':
+                    fish_tank_counts[2] += 1
+                elif tanks[i] == 'cube' and fish_perms[i] == 'bluegill':
+                    fish_tank_counts[3] += 1
+            ans.add(tuple(fish_tank_counts))
+    for a in ans:
+        print(f'round: {a[0]} bass, {a[1]} bluegill')
+        print(f'cube : {a[2]} bass, {a[3]} bluegill')
+        print()
+    print(len(ans))
