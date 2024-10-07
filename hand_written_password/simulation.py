894f74bb35b900c2d0c62aaa4eb7cd8bcfdd02e1
brianSalk <brian.salkas@gmail.com>
2024-10-07 19:04:12 +0000

    Added if __name__ == "main":

--- hand_written_password/simulation.py
+++ hand_written_password/simulation.py
@@ -6,21 +6,22 @@ char3_options = ['X', 'x']
 correct_password = 'II0OOXx'
 all_passwords = set()
 NUM_TRIALS = 2_500_000
-for _ in range(NUM_TRIALS//2):
-    stick1 = random.choice(char1_options)
-    stick2 = random.choice(char1_options)
-    circle1 = random.choice(char2_options)
-    circle2 = random.choice(char2_options)
-    circle3 = random.choice(char2_options)
-    cross1 = random.choice(char3_options)
-    cross2 = random.choice(char3_options)
-    all_passwords.add(stick1 + stick2 + circle1 + circle2 + circle3 + cross1 + cross2)
-print(f'there are a total of {len(all_passwords)} passwords')
+if __name__ == '__main__':
+    for _ in range(NUM_TRIALS//2):
+        stick1 = random.choice(char1_options)
+        stick2 = random.choice(char1_options)
+        circle1 = random.choice(char2_options)
+        circle2 = random.choice(char2_options)
+        circle3 = random.choice(char2_options)
+        cross1 = random.choice(char3_options)
+        cross2 = random.choice(char3_options)
+        all_passwords.add(stick1 + stick2 + circle1 + circle2 + circle3 + cross1 + cross2)
+    print(f'there are a total of {len(all_passwords)} passwords')
 
-correct_count = 0
-for _ in range(NUM_TRIALS):
-    correct_count += (correct_password in random.sample(list(all_passwords), k=3))
+    correct_count = 0
+    for _ in range(NUM_TRIALS):
+        correct_count += (correct_password in random.sample(list(all_passwords), k=3))
 
-print(f'Probability of guessing the correct password in 3 tries: {correct_count/NUM_TRIALS}')
+    print(f'Probability of guessing the correct password in 3 tries: {correct_count/NUM_TRIALS}')
 
 
