aad7da3c4954a319925b644e3e8aa66ceeddc06b
Brian Salkas <brian.salkas@gmail.com>
2025-02-10 13:06:53 -0600

    Create simulate.py

---
+++ the_dyslexic_brian/simulate.py
@@ -0,0 +1,23 @@
+from itertools import permutations, combinations
+from math import comb, factorial
+from copy import copy
+
+
+if __name__ == '__main__':
+    name = 'brian'
+    names = set()
+    for perm in permutations(name):
+        perm = list(perm)
+        for n in range(len(name)):
+            for cmb in combinations(range(len(name)),r=n):
+                perm_og = copy(perm)
+                for i in cmb:
+                    if perm[i] != 'i':
+                        perm_og[i] = perm_og[i].upper()
+                names.add(tuple(perm_og))
+                
+    eq = factorial(5) * 2**4
+    if eq != len(names):
+        print(f'{eq} does not equal {names}')
+    else:
+        print(eq)
