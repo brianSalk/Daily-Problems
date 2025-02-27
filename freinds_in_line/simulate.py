bff9aa40506cabbb67c36caca690eaa9e6224f59
Brian Salkas <brian.salkas@gmail.com>
2025-02-27 09:29:30 -0600

    Simplify has_no_isolated_freind function, create equation

--- freinds_in_line/simulate.py
+++ freinds_in_line/simulate.py
@@ -3,23 +3,25 @@
 # friends are standing next to another freind?
 # define a new constant E=(N-F)
 from itertools import permutations
+from math import perm
 
 
 def has_no_isolated_freind(l):
     l = ('E',) + l + ('E',)
-    for i,each in enumerate(l[1:-1]):
-        i+=1
+    for i,each in enumerate(l):
         if each == 'F':
             if l[i-1] != 'F' and l[i+1] != 'F':
                 return False
     return True
 
 N=10
-F=6
+F=5
 E=(N-F)
 line = ['F']*F + ['E']*E
 
 sim=0
+eq = 6 * perm(5)**2 + perm(6,2) * perm(5)**2
+print(eq)
 for perm in permutations(line):
     sim += has_no_isolated_freind(perm)
 print(sim)
