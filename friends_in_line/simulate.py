b6445d643dddff6797569056d218e8c0afed7b36
Brian Salkas <brian.salkas@gmail.com>
2025-02-27 09:35:49 -0600

    Paste updated version from old directory

--- friends_in_line/simulate.py
+++ friends_in_line/simulate.py
@@ -1,28 +1,29 @@
-
 # we have F freinds waiting in a line of N people.
 # how many ways can they form a line so that each of the F
 # friends are standing next to another freind?
 # define a new constant E=(N-F)
 from itertools import permutations
+from math import perm
 
 
-def has_no_isolated_freind(l):
+def has_no_isolated_friend(l):
     l = ('E',) + l + ('E',)
-    for i,each in enumerate(l[1:-1]):
-        i+=1
+    for i,each in enumerate(l):
         if each == 'F':
             if l[i-1] != 'F' and l[i+1] != 'F':
                 return False
     return True
 
 N=10
-F=6
+F=5
 E=(N-F)
 line = ['F']*F + ['E']*E
 
 sim=0
+eq = 6 * perm(5)**2 + perm(6,2) * perm(5)**2
+print(eq)
 for perm in permutations(line):
-    sim += has_no_isolated_freind(perm)
+    sim += has_no_isolated_friend(perm)
 print(sim)
 
 """
