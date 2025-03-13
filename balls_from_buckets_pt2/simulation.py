ac57022a8c51cb412e8ee376f8aadcb71112c4a6
Brian Salkas <brian.salkas@gmail.com>
2025-03-13 15:03:44 -0500

    Create simulation.py

---
+++ balls_from_buckets_pt2/simulation.py
@@ -0,0 +1,32 @@
+from math import comb
+from random import random
+p = 1/6
+r = 4
+k = 7
+n = 4+7
+"""
+Explanation of equation:
+comb(r+k-1, k): there are a total of r+k trials
+    but the last one must be a success, so we subtract 1
+p**r * (1-p)**k: r succsses and k failures, to the power of the number of each
+"""
+eq = comb(r+k-1, k) * p**r * (1-p)**k
+print("Probability of selecting 11 balls before getting 4 red")
+print("Equation:  ", eq)
+NUM_TRIALS = 2_000_000
+count = 0
+for _ in range(NUM_TRIALS):
+    s = 0
+    f = 0
+    while s + f < r+k:
+        if random() < p:
+            s+=1
+            if s == r and f == k:
+                count += 1
+                break
+        else:
+            f+=1
+print("Simulation:", count/NUM_TRIALS)
+print()
+
+print("Probability of selecting 3 of 6 red of 11 total")
